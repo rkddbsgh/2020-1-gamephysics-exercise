8ab06083f7146850cfc65aae028e617b62e96287
Jae Beom Kim <jbkim1@vision.hoseo.edu>
2020-05-27 13:06:17 +0900

    11주차 과제 셋업
    기본 스크립트
    
    벽추가

---
+++ AiSample/Assets/AiSample/week11/Agent11.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Agent11 : MonoBehaviour
+{
+    private Vector3 _pickPos = Vector3.zero;
+
+    private void Update()
+    {
+        if (Input.GetMouseButtonUp(0))
+        {
+            Vector3 mouse_pos = Input.mousePosition;
+
+            RaycastHit hit;
+            if (Physics.Raycast(Camera.main.ScreenToWorldPoint(mouse_pos), -Vector3.up, out hit, 1000))
+            {
+                _pickPos = hit.point;
+            }
+        }
+    }
+
+    private void OnDrawGizmos()
+    {
+        Gizmos.color = Color.red;
+        Gizmos.DrawWireSphere(_pickPos, 1.0f);
+    }
+}
